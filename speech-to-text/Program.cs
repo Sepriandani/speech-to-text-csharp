78016c3f7ccf50b9e441f02eebdfff6b5c8ca34f
sepriandani <sepriandanni@gmail.com>
2023-02-07 14:18:54 +0700

    speech to text with c#

---
+++ speech-to-text/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Speech.Recognition;
+
+namespace SpeechToText
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Inisialisasi engine speech recognition
+            using (SpeechRecognitionEngine recognizer = new SpeechRecognitionEngine(new System.Globalization.CultureInfo("id-ID")))
+            {
+                // Membuat grammar untuk mengakui kalimat dasar
+                GrammarBuilder grammarBuilder = new GrammarBuilder();
+                grammarBuilder.AppendDictation();
+                Grammar grammar = new Grammar(grammarBuilder);
+                recognizer.LoadGrammar(grammar);
+
+                // Mulai menerima masukan suara
+                Console.WriteLine("Berbicara sekarang.");
+                recognizer.SetInputToDefaultAudioDevice();
+                RecognitionResult result = recognizer.Recognize();
+
+                // Menampilkan teks hasil analisis
+                Console.WriteLine("Anda berkata: " + result.Text);
+            }
+        }
+    }
+}
